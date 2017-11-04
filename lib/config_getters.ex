fffb7582f5d41dd9940cc8b93c4ce4e0e6a76437
Joao Bernardo <jb.amaro95@gmail.com>
2017-11-04 00:37:26 +0000

    Implements config_getters macro
    This macro generates config getter functions from a given app name and
    a list of config keys.

--- lib/config_getters.ex
+++ lib/config_getters.ex
@@ -1,18 +1,19 @@
 defmodule ConfigGetters do
-  @moduledoc """
-  Documentation for ConfigGetters.
-  """
+  defmacro __using__(_opts) do
+    quote do
+      import ConfigGetters
+    end
+  end
 
-  @doc """
-  Hello world.
+  defmacro config_getters(app, keys) do
+    Enum.map(keys, &(generate_getter(app, &1)))
+  end
 
-  ## Examples
-
-      iex> ConfigGetters.hello
-      :world
-
-  """
-  def hello do
-    :world
+  defp generate_getter(app, key) when is_atom(key) do
+    quote do
+      defp unquote(key)() do
+        Application.get_env(unquote(app), unquote(key))
+      end
+    end
   end
 end
--- test/config_getters_test.exs
+++ test/config_getters_test.exs
@@ -1,8 +1,27 @@
 defmodule ConfigGettersTest do
   use ExUnit.Case
-  doctest ConfigGetters
 
-  test "greets the world" do
-    assert ConfigGetters.hello() == :world
+  defmodule ExampleModule do
+    use ConfigGetters
+
+    config_getters :config_getters, [:key1, :key2, :key3]
+
+    # Config getter functions are private, so they cannot be called outside the module
+    # These functions are defined to make it possible to call them from outside
+    def key1_getter, do: key1()
+    def key2_getter, do: key2()
+    def key3_getter, do: key3()
+  end
+
+  setup do
+    Application.put_env(:config_getters, :key1, :value1)
+    Application.put_env(:config_getters, :key2, :value2)
+    Application.put_env(:config_getters, :key3, :value3)
+  end
+
+  test "generated getter methods returns the right values" do
+    assert :value1 = ExampleModule.key1_getter
+    assert :value2 = ExampleModule.key2_getter
+    assert :value3 = ExampleModule.key3_getter
   end
 end
