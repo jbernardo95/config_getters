47353ac51327f9a6d352c46ee6e8137cfe97e3e0
Joao Bernardo <jb.amaro95@gmail.com>
2017-11-04 00:51:38 +0000

    Adds documentation to ConfigGetters module

--- lib/config_getters.ex
+++ lib/config_getters.ex
@@ -1,10 +1,28 @@
 defmodule ConfigGetters do
+  @moduledoc """
+  A module for generating cofing getters.
+
+  ### Example
+
+      defmodule ApplicationModule do
+        use ConfigGetters
+
+        config_getters :my_app, [:key1, :key2, :key3]
+
+        # Access config values using their key name
+        # For example key1() will return key1 config value
+      end
+  """
+
   defmacro __using__(_opts) do
     quote do
       import ConfigGetters
     end
   end
 
+  @doc """
+  Generates application config getters for the given application and keys.
+  """
   defmacro config_getters(app, keys) do
     Enum.map(keys, &(generate_getter(app, &1)))
   end
